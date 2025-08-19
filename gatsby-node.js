e74319c4bb42b444e758a542bba0f91f85fb7ff4
nawahtkui <alnwyra@gmail.com>
2025-08-19 08:57:01 +0300

    Add files via upload

---
+++ gatsby-node.js
@@ -0,0 +1,4 @@
+// يمكنك إضافة إعدادات Gatsby Node هنا
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions;
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/main.css";
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1>مرحبًا بكم في NawahPI</h1>
+      </header>
+      <main>
+        <p>هذا هو الموقع الجديد المبني بـ Gatsby وReact.</p>
+      </main>
+    </div>
+  );
+}
